e76ae0c135b351e19bedb0739bb6be6cca9cca09
Nathaniel Itty <nitty0225@students.ossiningufsd.org>
2019-02-05 00:00:55 -0500

    Modified for 4122
    We fixed the gyro code and modified the code to be used for Megabyte

--- 7004_Timed/0125_Pixel - V3/src/main/cpp/Robot.cpp
+++ 7004_Timed/0125_Pixel - V3/src/main/cpp/Robot.cpp
@@ -2,7 +2,7 @@
 
 void Robot::RobotInit() {
     camera= CameraServer::GetInstance()->StartAutomaticCapture();
-    camera.SetResolution(640,480);
+    camera.SetResolution(426,240);
     camera.SetExposureAuto();
     camera.SetFPS(30);
     camera.SetBrightness(20);
@@ -21,39 +21,40 @@ void Robot::TeleopInit() {
 }
 void Robot::TeleopPeriodic() {
     Wait(2);
-    myGyro.SetYaw(0);
     myGyro.GetYawPitchRoll(gyro);
+    myGyro.SetYaw(0);
+    double sens = (stick2.GetZ() + 1) / 2; //sensitivity 
+
      while(IsOperatorControl() && IsEnabled()) {
          visionlight.Set(frc::Relay::Value::kOn);
 
-         if (stick2.GetRawButton(2)) {
-            myGyro.GetYawPitchRoll(gyro);
-            if (stick2.GetRawButton(1)){
+
+            if (stick2.GetRawButton(2)) {
+                myGyro.GetYawPitchRoll(gyro);
+                if (stick2.GetRawButton(1)){
                 myGyro.SetYaw(0);
-		ant = gyro[0]
-	    }
-	    else {
-		ant = gyro[0];
-	    }
-            while (gyro[0] < ant + .05 && gyro[0] > ant - .05 && stick2.GetRawButton(2)) {
+                ant = (gyro[0]);
+            }
+
+            if (gyro[0] < ant + .05 && gyro[0] > ant - .05 && stick2.GetRawButton(2)) {
                 m_left.Set(A*stick2.GetY());
                 m_right.Set(x*stick2.GetY());
                 m_updown.Set(.65*stick3.GetY());
             }
-		if (gyro[0] > ant + 0.5) {
-                x = x - .01;
-		}
-		else if (gyro[0] < ant - 0.5) {
-		A = A - .01;
-		}
-		m_left.Set(A*stick2.GetY());
-                m_right.Set(x*stick2.GetY());
-                m_updown.Set(.65*stick3.GetY());
+			else if (gyro[0] > ant + 0.5) {
+                x = x - .001;
+			}
+			else if (gyro[0] < ant - 0.5) {
+				A = A - .001;
+			}
+            m_left.Set(A*stick2.GetY());
+            m_right.Set(x*stick2.GetY());
+            m_updown.Set(0.4*stick3.GetY());
          }	
          else {
-            m_left.Set(-0.65*stick1.GetY()); //left wheels
-            m_right.Set(-0.79*stick2.GetY()); //right wheels
-            m_updown.Set(0.65*stick3.GetY()); //arm controls
+            m_left.Set(-((stick2.GetZ()+1)/2) * stick1.GetY()); //left wheels
+            m_right.Set(-((stick2.GetZ()+1)/2) * stick2.GetY()); //right wheels
+            m_updown.Set(0.65 * stick3.GetY()); //arm controls
             while(stick3.GetRawButton(1)) { //arm wheels
                 if (stick3.GetRawButton(2)) {
                     wheels.Set(0.99); //outake
@@ -78,7 +79,8 @@ void Robot::TeleopPeriodic() {
             if (stick3.GetRawButton(9)){
                 TeleopPeriodic();
             }
-        }
+            smartDashboard -> PutNumber("Throttle", sens);
+         }
      }
 
 }
